9396cb50b62c05b5b4394e40ab92ca722cf6a65f
Zeeshan ahmad <ahmadzee26@gmail.com>
2020-08-21 22:04:59 +0500

    Create Project_managment.js

---
+++ Project_managment.js
@@ -0,0 +1,28 @@
+#Starting a new project
+npm init
+
+npm init -y
+
+
+#installing library in the project
+npm i libraryname
+
+#installing library globaly 
+npm i -g library name 
+
+#running a script
+node index.js #index.js can by any js file 
+
+#running with esm
+#install esm
+npm i esm
+
+#running with esm 
+node -r esm main.js #main.js can be any file 
+
+#running with the start
+npm start 
+
+
+
+
