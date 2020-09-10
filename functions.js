efca7b0b22b05d557fae8ccb297bdaa503dc02ae
Zeeshan ahmad <ahmadzee26@gmail.com>
2020-09-11 00:20:39 +0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
+#arrow function
 
 const sequre = (x) => {
   return x * x
 }
+console.log(sequre(3))
+
+
+#arrow function with only one statement
+const sequre = (x) => x * x
 
 console.log(sequre(3))
